65863c6c8553eebda16bc6f81dc4867394c33fb1
raju <rajugupta5518@gmail.com>
2016-11-28 19:32:54 +0530

    chek wq

--- app/Test.php
+++ app/Test.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model {
-
+// this
     protected $fillable = ['name', 'email', 'address'];
 
     public function order() {
