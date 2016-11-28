545d559eb2fa4cddc7b0e9e299df0bcef9694e54
WhileTechnology <rajugupta5518@gmail.com>
2016-11-28 19:35:25 +0530

    Update Test.php

--- app/Test.php
+++ app/Test.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model {
-// this
+// this from gits 
     protected $fillable = ['name', 'email', 'address'];
 
     public function order() {
